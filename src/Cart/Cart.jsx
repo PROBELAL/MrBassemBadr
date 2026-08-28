c7f36a7fe2c0cfd45da3e2a7227573b39c909e0e
PROBELAL <belal.badr001@gmail.com>
2026-08-28 07:17:12 +0300

    fix massage

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -5,12 +5,20 @@ import CartItem from "../CartItem/CartItem";
 const Cart = () => {
     const cartItems = useSelector((state) => state.cartReducer.items);
     const total_cost = cartItems.reduce((total, item) => total + item.price, 0);
-    const products_names = cartItems.map((Element)=>Element.title);
+    const products_info = cartItems.map((Element) => {
+    return {
+        title: Element.title, 
+        price: Element.price
+    };
+    });
+    const products_names = cartItems.map((Element) => {
+    return `${Element.title} (${Element.price}$)`; 
+    });
     const handleConfirmation=()=>{
         if(total_cost>0){
             const phoneNumber = "201003538925";
-            const names=products_names.join(" و ");
-            const message =`لقد طلبت ${names} والسعر الاجمالى ${total_cost}$`;
+            const names_price=products_names.join(" و ");
+            const message =`لقد طلبت ${names_price} والسعر الاجمالى ${total_cost}$`;
             const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
             window.open(url, '_blank'); 
         }
